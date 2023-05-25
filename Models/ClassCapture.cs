6e75636108afa0dfc39190eff99b0bb67c369b20
jeqiMaham <mahamguziyev@gmail.com>
2023-05-24 17:11:34 -0700

    1th commit

---
+++ Models/ClassCapture.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+
+namespace StreamAtLesson_8May.Models
+{
+    public  class ClassCapture
+    {
+        public static Bitmap CaptureScreen()
+        {
+            Rectangle bounds = ScreenBounds();
+            Bitmap screenshot = new Bitmap(bounds.Width, bounds.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            using (Graphics graphics = Graphics.FromImage(screenshot))
+            {
+                graphics.CopyFromScreen(bounds.Left, bounds.Top, 0, 0, bounds.Size);
+            }
+
+            return screenshot;
+        }
+
+        static Rectangle ScreenBounds()
+        {
+            IntPtr screen = GetDesktopWindow();
+            GetWindowRect(screen, out RECT bounds);
+            return new Rectangle(bounds.Left, bounds.Top, bounds.Right - bounds.Left, bounds.Bottom - bounds.Top);
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDesktopWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+
+    }
+
+  
+
+}
+
+/////////////////////
+
+
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using StreamAtLesson_8May.Models;
+
+
+namespace StreamAtLesson_8May
+{
+   
+
+    public class Program
+    {
+        static void Main()
+        {
+            int say = 0;
+            while (true)
+            {
+                Console.WriteLine("Screen etmek , yoxsa GetPictures sechmek isteyirsiniz ? ( A  ve ya  B daxil edin) :  ");
+                dynamic choose = Console.ReadKey();
+                if (choose.Key! == ConsoleKey.A)
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Ekran Resmi almaq uchun Enter , cixmaq uchun istenilen diger symbol daxil edin ");
+
+                        if (Console.ReadKey().Key == ConsoleKey.Enter)
+                        {
+                            if (say == 0) Directory.CreateDirectory($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/ScreensByCs");
+
+                            FileStream stream = new($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/ScreensByCs/screenshot_.{DateTime.Now:yyyyMMddHHmmss}.png", FileMode.OpenOrCreate, FileAccess.Write);
+
+
+                            
+
+                            Bitmap screenshot = ClassCapture.CaptureScreen();
+                            screenshot.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                            // screenshot.Save (,)
+                            Console.WriteLine("Ekran görüntüsü saxlanildi: " + stream);
+                        }
+                        else break;
+                    }
+                }
+                else if (choose.Key == ConsoleKey.B)
+                {
+                    string folderPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/ScreensByCs";
+                    string[] photoFiles = Directory.GetFiles(folderPath, "*.png"); 
+
+                    foreach (string photoFile in photoFiles)
+                    {
+                        using (FileStream fs = File.OpenRead(photoFile))
+                        {
+                           
+                            Console.WriteLine(Path.GetFileName(photoFile));
+                        }
+                    }
+                    Console.ReadKey(true );
+                }
+                else continue;
+                break;
+            }
+        }
+        
+    }
+
+}
+
+
+
+/*
+ Her defe Enter duymesine click edende ekranin ScreenShot-nu ceksin
+ve hemin ScreenShot-lari Desktop-da Images Folder-inde save etsin.
+Eger Images folder-i yoxdusa yaransin, varsa yaranmasin.
+
+Her defe shekil save edende, kohne shekiller silinmesin.
+Yazdiginiz kod menim komputerim de de ishlemelidir.
+
+Elave:
+
+Hemin folder-in icherisindeki shekillerin adlarini chap etmek funskiyanalligi da olsun.
+GetFiles()....
+
+Eger alindira bilsez, hansisa shekili sechende onu Default Program-i ile achsin. (Photos)
+ */
